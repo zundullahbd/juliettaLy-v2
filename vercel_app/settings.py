1eeab4f993aefab47e21f2655686538d97997d54
zundullahbd <uluzundullah@gmail.com>
2023-04-07 23:27:10 +0700

    testing

--- vercel_app/settings.py
+++ vercel_app/settings.py
@@ -124,6 +124,6 @@ STATIC_URL = 'static/'
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 if os.environ.get('VERCEL'):
-    STATICFILES_DIRS = os.path.join(BASE_DIR, 'static'),
+    STATICFILES_DIRS = os.path.join(BASE_DIR, 'staticfiles'),
     #STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles_build', 'static')
     STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
